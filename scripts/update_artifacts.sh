bd9a8882888d409b7db9a462ee63e33eb2c24036
Oscar Dowson <o.dowson@gmail.com>
2025-12-18 08:15:35 +1300

    Add support for v15.1.0

--- scripts/update_artifacts.sh
+++ scripts/update_artifacts.sh
@@ -9,7 +9,8 @@ mkdir /tmp/libknitro
 cd /tmp/libknitro
 
 mkdir exdir
-curl https://files.pythonhosted.org/packages/7e/05/faf197e94566bfd18124e3bec5e8d5b443104ee7ece979add309c69ee4db/knitro-15.0.1-py3-none-macosx_13_0_arm64.whl -o tmp.zip
+
+curl https://files.pythonhosted.org/packages/35/b4/2fe70abca447113efdbda7bea2f87d55e4aa1101da7da2af4437014d6059/knitro-15.1.0-py3-none-macosx_13_0_arm64.whl -o tmp.zip
 unzip tmp.zip -d exdir
 cp exdir/knitro-*/licenses/LICENSE exdir/knitro/LICENSE
 rm -rf exdir/knitro/scipy
@@ -22,7 +23,7 @@ rm -rf knitro
 rm tmp.zip
 
 mkdir exdir
-curl https://files.pythonhosted.org/packages/49/52/39b3c752de920ce41b6bfa47e7732406c734c4d68e0bc0464a595f6cffa9/knitro-15.0.1-py3-none-win_amd64.whl -o tmp.zip
+curl https://files.pythonhosted.org/packages/da/c5/543ec0e5f11ed965ac59e6003eeecbf8d7c6201baade717981bbd59950c5/knitro-15.1.0-py3-none-win_amd64.whl -o tmp.zip
 unzip tmp.zip -d exdir
 cp exdir/knitro-*/LICENSE exdir/knitro/LICENSE
 rm -rf exdir/knitro/scipy
@@ -35,7 +36,7 @@ rm -rf knitro
 rm tmp.zip
 
 mkdir exdir
-curl https://files.pythonhosted.org/packages/8a/ea/b172c8c43ebb072fb54fefb5f3b6eb16bc56e2babc66d0e084a3083c3ec4/knitro-15.0.1-py3-none-manylinux1_x86_64.whl -o tmp.zip
+curl https://files.pythonhosted.org/packages/0d/de/dc56dd6c368fdcda8227c026d792095e1b49f9d3ef6ebccc3b2118a92147/knitro-15.1.0-py3-none-manylinux_2_28_x86_64.whl -o tmp.zip
 unzip tmp.zip -d exdir
 cp exdir/knitro-*/licenses/LICENSE exdir/knitro/LICENSE
 rm -rf exdir/knitro/scipy
@@ -43,6 +44,19 @@ rm -rf exdir/knitro/numpy
 rm -rf exdir/knitro/*.py
 mv exdir/knitro knitro
 tar -cjf x86_64-linux-gnu.tar.bz2 knitro
+rm -rf exdir
+rm -rf knitro
+rm tmp.zip
+
+mkdir exdir
+curl https://files.pythonhosted.org/packages/53/c4/716861f408ca9cd6d25e36ee81f369ba7f7e84cbfa458c830f4abb2b3954/knitro-15.1.0-py3-none-manylinux_2_28_aarch64.whl -o tmp.zip
+unzip tmp.zip -d exdir
+cp exdir/knitro-*/licenses/LICENSE exdir/knitro/LICENSE
+rm -rf exdir/knitro/scipy
+rm -rf exdir/knitro/numpy
+rm -rf exdir/knitro/*.py
+mv exdir/knitro knitro
+tar -cjf aarch64-linux-gnu.tar.bz2 knitro
 rm -rf exdir
 rm -rf knitro
 rm tmp.zip
